9e16663c1ba2721b61b398a0ba1ca14a955d908f
NazirLattimore0 <nlattim1@email.cpcc.edu>
2023-01-31 11:04:41 -0500

    adding contact form

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import { Home } from "./components/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Footer } from "./components/footer";
 import Membership from "./components/Memership";
+import Contact from "./components/contact";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/AboutPage" element={<AboutPage />}></Route>
           <Route path="/Membership" element={<Membership />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
         </Routes>
       </BrowserRouter>
       <Footer />
